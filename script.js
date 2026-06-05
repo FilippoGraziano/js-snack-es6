27736a0892e6dc0851ac3f227f7423adb00b9a3b
Filippo Graziano <filippograziano13@gmail.com>
2026-06-05 16:40:55 +0200

    add array of bikes

---
+++ script.js
@@ -0,0 +1,28 @@
+const bikes = [
+
+    {
+        name : `mountain bike`,
+        weightKg : 14
+    },
+
+    {
+        name : `city bike`,
+        weightKg : 10
+    },
+
+    {
+        name : `run bike`,
+        weightKg : 10
+    },
+
+    {
+        name : `gravel bike`,
+        weightKg : 11
+    },
+
+    {
+        name : `city e-bike`,
+        weightKg : 20
+    }
+    
+]
