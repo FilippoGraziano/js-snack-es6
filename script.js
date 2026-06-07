3b08b831eb02b9023a39aaee7df55ccd85636166
Filippo Graziano <filippograziano13@gmail.com>
2026-06-07 09:29:12 +0200

    create array of soccer teams with random number

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const bikes = [
         weightKg : 20
     }
     
-]
+];
 
 let lowerKgBikeName = ``;
 let lowerKgBike = bikes[0].weightKg;
@@ -41,8 +41,50 @@ for (const element of bikes) {
 }
 
 console.log(lowerKgBikeName, lowerKgBike);
+
 const bikeLowerKg = document.createElement(`div`);
-bikeLowerKg.innerHTML = `La <strong>${lowerKgBikeName}</strong> è quella più leggera e pesa <strong>${lowerKgBike} Kg</strong>`
+bikeLowerKg.innerHTML = `La <strong>${lowerKgBikeName}</strong> è quella più leggera e pesa <strong>${lowerKgBike} Kg</strong>`;
 
 const body = document.querySelector(`body`);
-body.appendChild(bikeLowerKg)
+body.appendChild(bikeLowerKg);
+
+//r second snack
+
+const randomNumber = () => {
+
+    const number = Math.floor(Math.random() * 11);
+    return number;
+};
+
+const soccerTeams = [
+
+    {
+        name : `Juventus`,
+        pointScored : randomNumber(),
+        faulsSuffered : randomNumber(),
+    },
+
+    {
+        name : `Inter`,
+        pointScored : randomNumber(),
+        faulsSuffered : randomNumber(),
+    },
+    
+    {
+        name : `Milan`,
+        pointScored : randomNumber(),
+        faulsSuffered : randomNumber(),
+    },
+
+    {
+        name : `Lazio`,
+        pointScored : randomNumber(),
+        faulsSuffered : randomNumber(),
+    },
+
+    {
+        name : `Roma`,
+        pointScored : randomNumber(),
+        faulsSuffered : randomNumber(),
+    }
+];
