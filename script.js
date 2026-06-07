348a06d4a48494fb1e2977541ef67e80c0e75a39
Filippo Graziano <filippograziano13@gmail.com>
2026-06-07 09:11:00 +0200

    Implements logic to identify lightest bike
    Adds JavaScript functionality to iterate through a list of bikes and determine the one with the lowest weight. Displays the name and weight of the lightest bike directly on the web page, enhancing user experience by providing immediate feedback.

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ const bikes = [
     }
     
 ]
+
+let lowerKgBikeName = ``;
+let lowerKgBike = bikes[0].weightKg;
+for (const element of bikes) {
+
+    if (element.weightKg < lowerKgBike) {
+
+        lowerKgBike = element.weightKg;
+        lowerKgBikeName = element.name
+    }
+}
+
+console.log(lowerKgBikeName, lowerKgBike);
+const bikeLowerKg = document.createElement(`div`);
+bikeLowerKg.innerHTML = `La <strong>${lowerKgBikeName}</strong> è quella più leggera e pesa <strong>${lowerKgBike} Kg</strong>`
+
+const body = document.querySelector(`body`);
+body.appendChild(bikeLowerKg)
